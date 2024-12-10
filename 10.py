acefa82eb73e11a27b7063fd8e2ed59646626691
GinOwO <65128789+GinOwO@users.noreply.github.com>
2024-12-10 12:45:01 +0530

    day 10 update

--- 10.py
+++ 10.py
@@ -1,4 +1,6 @@
-from typing import List, Tuple, Set
+from collections import defaultdict
+from typing import DefaultDict, List, Set, Tuple
+
 from helperlib.constants import DIRS4
 
 
@@ -9,15 +11,13 @@ def preprocess() -> List[List[int]]:
 
 
 def partA(map: List[List[int]]):
-    reachable: List[List[Set[Tuple[int, int]]]] = [
-        [-1 for j in range(len(map[i]))] for i in range(len(map))
-    ]
+    reachable: DefaultDict[int, DefaultDict[int, Set[Tuple[int, int]]]] = defaultdict(
+        lambda: defaultdict(lambda: -1)
+    )
 
     def walk(x: int, y: int) -> Set[Tuple[int, int]]:
         if map[x][y] == 9:
-            a = set()
-            a.add((x, y))
-            return a
+            return {(x, y)}
 
         if reachable[x][y] != -1:
             return reachable[x][y]
@@ -29,7 +29,7 @@ def partA(map: List[List[int]]):
                 a >= 0
                 and b >= 0
                 and a < len(map)
-                and b < len(map[0])
+                and b < len(map[a])
                 and map[a][b] == map[x][y] + 1
             ):
                 reachable[x][y] |= walk(a, b)
@@ -45,7 +45,9 @@ def partA(map: List[List[int]]):
 
 
 def partB(map: List[List[int]]):
-    ratings: List[List[int]] = [[-1] * len(map[0]) for _ in range(len(map))]
+    ratings: DefaultDict[int, DefaultDict[int, int]] = defaultdict(
+        lambda: defaultdict(lambda: -1)
+    )
 
     def walk(x: int, y: int):
         if map[x][y] == 9:
